12d3f45b952ca060f71e0111da450bea8ebc8926
mahmoudelmekwed <mahmoud.elmekwed@gmail.com>
2024-01-08 23:29:33 +0200

    add some styling to nav bar

--- app.py
+++ app.py
@@ -1,8 +1,44 @@
 import flask
-from flask import render_template
+import json
+from flask import render_template , jsonify , request
 from datetime import datetime
 
+
 app = flask.Flask("Online Store")
+
+def load_products():
+    file = open('products.json', 'r')
+    products = json.load(file)
+    return products
+
+def save_products(products):
+    file = open('products.json', 'w')
+    json.dump(products, file)
+
+class Product:
+    def __init__(self , id , image ,  name , price , description , stock):
+        
+        self.id = id
+        self.image = image
+        self.name = name
+        self.price = price
+        self.description = description
+        self.stock = stock
+
+    def discount(self , discount_percentage):
+        self.price = self.price * (100 - discount_percentage) / 100
+
+    def update_stock (self , quantity):
+        try:
+            new_stock = self.stock + quantity
+
+            if new_stock < 0:
+                return "Stock quantity can not be negative"
+            
+            self.stock = new_stock 
+            return "Stock updated successfully."
+        except:
+            return "Quantiy must be a number"
 
 @app.route("/")
 def home():
@@ -16,4 +52,10 @@ def home():
     remaining_seconds = remaining_seconds % 3600
     minutes = remaining_seconds // 60
     seconds = remaining_seconds % 60
-    return render_template("index.html" , days = days , hours = hours , minutes = minutes , seconds = seconds)
+    products = load_products()
+    return render_template("index.html" , days =days , hours = hours , minutes = minutes , seconds = seconds , products=products)
+
+# @app.route("/products")
+# def get_products():
+#     products = load_products()
+#     return render_template("index.html", products=products)
