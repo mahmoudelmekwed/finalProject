33924e33e9d84441e7f59bcacc72c38d2440b79a
mahmoudelmekwed <mahmoud.elmekwed@gmail.com>
2024-01-08 10:59:41 +0200

    add header and nav bar to my site

--- app.py
+++ app.py
@@ -1,13 +1,8 @@
 import flask
+from flask import render_template
 
 app = flask.Flask("Online Store")
 
-def get_html(page_name):
-    html_file = open(page_name + ".html")
-    content = html_file.read()
-    html_file.close()
-    return content
-
 @app.route("/")
 def home():
-    return get_html("index")
+    return render_template("index.html")
