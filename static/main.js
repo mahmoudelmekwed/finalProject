1f5bbe21a1d2239d836568a1a25cb502795444d7
mahmoudelmekwed <mahmoud.elmekwed@gmail.com>
2024-01-13 23:38:41 +0200

    add total price to product detail page

--- static/main.js
+++ static/main.js
@@ -1,8 +1,8 @@
 
 function updateTotalPrice(productId) {
     console.log("updateTotalPrice called for product ID:", productId);
-    var pricePerUnit = document.getElementById('price-'+productId).innerText;
-    var quantity = document.getElementById('quantity-'+productId).value;
-    var totalPrice = pricePerUnit * quantity;
+    let pricePerUnit = document.getElementById('price-'+productId).innerText;
+    let quantity = document.getElementById('quantity-'+productId).value;
+    let totalPrice = pricePerUnit * quantity;
     document.getElementById('total_price-'+productId).innerText = totalPrice;
 }
