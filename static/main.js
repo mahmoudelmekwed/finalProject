00d7a2cf5eb278bd9c162bb78cbac86c20a98e45
mahmoudelmekwed <mahmoud.elmekwed@gmail.com>
2024-01-14 10:23:29 +0200

    adjusting total price in product detail page

--- static/main.js
+++ static/main.js
@@ -3,5 +3,5 @@ function updateTotalPrice(productId) {
     let pricePerUnit = document.getElementById('price-'+productId).getAttribute('data-price-per-unit');
     let quantity = document.getElementById('quantity-'+productId).value;
     let totalPrice = pricePerUnit * quantity;
-    document.getElementById('price-'+productId).innerText = totalPrice.toFixed(2);
+    document.getElementById('price-'+productId).innerText = 'Total price: ' + totalPrice.toFixed(2);
 }
