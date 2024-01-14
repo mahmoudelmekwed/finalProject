e3328b523614937e93e73cf2eb69b66cc8a2b00b
mahmoudelmekwed <mahmoud.elmekwed@gmail.com>
2024-01-14 16:24:18 +0200

    edit in synchronization between quantity in pages

--- static/main.js
+++ static/main.js
@@ -1,27 +1,25 @@
-
 function updateTotalPrice(productId) {
-    let pricePerUnit = document.getElementById('price-'+productId).getAttribute('data-price-per-unit');
-    let quantity = document.getElementById('quantity-'+productId).value;
-    localStorage.setItem('quantity-'+productId , quantity);
+    let pricePerUnit = parseFloat(document.getElementById('price-'+productId).getAttribute('data-price-per-unit'));
+    let quantity = parseInt(document.getElementById('quantity-'+productId).value);
     let totalPrice = pricePerUnit * quantity;
     document.getElementById('price-'+productId).innerText = 'Total price: ' + totalPrice.toFixed(2);
+    localStorage.setItem('quantity-'+productId , quantity);
 }
 
-function onChangeQuantity(productId){
-    let quantity = document.getElementById('quantity-'+productId).value;
-    localStorage.setItem('quantity-'+productId , quantity);
-    updateTotalPrice(productId);
+
+function checkForUpdates() {
+    let productIds = [1,2,3,4,5,6,7,8,9]; 
+    productIds.forEach(function(productId) {
+        let savedQuantity = localStorage.getItem('quantity-' + productId);
+        let quantityElement = document.getElementById('quantity-' + productId);
+        if (savedQuantity && quantityElement && quantityElement.value !== savedQuantity) {
+            quantityElement.value = savedQuantity;
+            updateTotalPrice(productId);
+        }
+    });
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-    var productIds = [1,2,3,4,5,6,7,8,9];
-    productIds.forEach(function(productId) {
-        var savedQuantity = localStorage.getItem('quantity-' + productId);
-        if (savedQuantity) {
-            var quantityElement = document.getElementById('quantity-' + productId);
-            if (quantityElement) {
-                quantityElement.value = savedQuantity;
-            }
-        }
-    });
+    checkForUpdates();
+    setInterval(checkForUpdates, 1000); 
 });
