6ba95533badf3934606a736c3db37a4aeb05f040
mahmoudelmekwed <mahmoud.elmekwed@gmail.com>
2024-01-14 14:21:58 +0200

    synchronize product quantity between home and product list item

--- static/main.js
+++ static/main.js
@@ -2,6 +2,26 @@
 function updateTotalPrice(productId) {
     let pricePerUnit = document.getElementById('price-'+productId).getAttribute('data-price-per-unit');
     let quantity = document.getElementById('quantity-'+productId).value;
+    localStorage.setItem('quantity-'+productId , quantity);
     let totalPrice = pricePerUnit * quantity;
     document.getElementById('price-'+productId).innerText = 'Total price: ' + totalPrice.toFixed(2);
 }
+
+function onChangeQuantity(productId){
+    let quantity = document.getElementById('quantity-'+productId).value;
+    localStorage.setItem('quantity-'+productId , quantity);
+    updateTotalPrice(productId);
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    var productIds = [1,2,3,4,5,6,7,8,9];
+    productIds.forEach(function(productId) {
+        var savedQuantity = localStorage.getItem('quantity-' + productId);
+        if (savedQuantity) {
+            var quantityElement = document.getElementById('quantity-' + productId);
+            if (quantityElement) {
+                quantityElement.value = savedQuantity;
+            }
+        }
+    });
+});
