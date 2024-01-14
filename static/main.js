979066c47d0de53d689204276ba688c267599a72
mahmoudelmekwed <mahmoud.elmekwed@gmail.com>
2024-01-14 10:07:40 +0200

    adjust the product list to total prce only not price per unit

--- static/main.js
+++ static/main.js
@@ -1,8 +1,7 @@
 
 function updateTotalPrice(productId) {
-    console.log("updateTotalPrice called for product ID:", productId);
-    let pricePerUnit = document.getElementById('price-'+productId).innerText;
+    let pricePerUnit = document.getElementById('price-'+productId).getAttribute('data-price-per-unit');
     let quantity = document.getElementById('quantity-'+productId).value;
     let totalPrice = pricePerUnit * quantity;
-    document.getElementById('total_price-'+productId).innerText = totalPrice;
+    document.getElementById('price-'+productId).innerText = totalPrice.toFixed(2);
 }
